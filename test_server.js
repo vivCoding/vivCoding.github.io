d2795d8458025df2d3de1b58dd08362d161e9123
vuser <vivCoding@gmail.com>
2021-05-12 13:06:19 -0400

    fit to mobile better, color touch ups

---
+++ test_server.js
@@ -0,0 +1,29 @@
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
+
+const port = 3000;
+const home = __dirname + "/index.html";
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    if (req.url == "/") {
+        fs.readFile(home, (error, data) => {
+            res.statusCode = 200;
+            res.write(data);
+            res.end();
+        })
+    } else {
+        fs.readFile(__dirname + req.url, (error, data) => {
+            if (!error) {
+                res.statusCode = 200;
+                res.write(data);
+                res.end();
+            }
+        })
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
