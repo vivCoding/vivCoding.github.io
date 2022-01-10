8ea030322c0207f5994da7f5f84f7342bd6755ca
Vincent V <70539556+vivCoding@users.noreply.github.com>
2022-01-10 00:40:51 -0500

    Update data.js

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ var content = {
             title: "VChess",
             link: "https://vivcoding.github.io/vchess/",
             image: "assets/chess_piece_knight.png",
-            description: "A basic C++ chess engine for playing chess games and generating optimal moves. Designed to be paired with a GUI and be a backend for chess programs."
+            description: "A basic C++ chess engine for playing chess games and generating optimal moves. Designed to be paired with a GUI and be a backend for chess programs. Deployable to the web with WebAssembly."
         },
         {
             title: "Vusic",
