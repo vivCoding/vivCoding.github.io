f3dac53b9932a5e89f5a3a8df338b0afb7a215f2
vivCoding <vivCoding@gmail.com>
2023-11-20 02:01:28 -0500

    jazz

--- scripts/index.js
+++ scripts/index.js
@@ -22,14 +22,16 @@ document.body.onload = async () => {
   logoTop.style.strokeDasharray = 300
   logoStroke.style.strokeDasharray = 300
 
-  // logo step 1
+  const containerBr = heroText.getBoundingClientRect()
+  const dist = containerBr.height / 2
+  heroTitle.style.transform = `translateY(calc(${dist}px - 50%))`
 
+  heroText.style.opacity = 0
+  heroDesc.style.opacity = 0
+  heroBorder.style.transform = "scaleY(0)"
+
+  // step 1
   await waitForAnimations([
-    // logoInit.animate([{ scale: 0.8 }, { scale: 1 }], {
-    //   duration: 1000,
-    //   easing: "ease",
-    //   fill: "forwards",
-    // }),
     logoInit.animate(
       [{ transform: "translateY(-10px)" }, { transform: "translateY(0)" }],
       {
@@ -61,10 +63,7 @@ document.body.onload = async () => {
     ),
   ])
 
-  // start hero text animation concurrently, and don't wait to finish
-  heroTextAnimation()
-
-  // logo step 2
+  // step 2
   await waitForAnimations([
     logoInit.animate(
       [
@@ -130,7 +129,7 @@ document.body.onload = async () => {
     ),
   ])
 
-  // logo step 3
+  // step 3
   await waitForAnimations([
     logoInit.animate(
       {
@@ -143,44 +142,6 @@ document.body.onload = async () => {
         fill: "forwards",
       }
     ),
-  ])
-
-  // reset styling
-  logoInit.style.left = "0%"
-  logoInit.style.translate = "0%"
-
-  logoMiddle.style.fillOpacity = 1
-  logoBottom.style.fillOpacity = 1
-
-  // using animation fill forward to retain new transforms and adding hover effects afterwards is kinda whack
-  // hacky fix: just replace the node
-  const newNode = logoInit.cloneNode(true)
-  // change id to use hover effects defined in css
-  newNode.id = "logo"
-  // remove old node and replace with new one
-  const parent = logoInit.parentNode
-  const sibling = logoInit.nextSibling
-  logoInit.remove()
-  parent.insertBefore(newNode, sibling)
-}
-
-async function heroTextAnimation() {
-  // await delay(200)
-  // initial setup
-  const containerBr = heroText.getBoundingClientRect()
-  const dist = containerBr.height / 2
-  heroTitle.style.transform = `translateY(calc(${dist}px - 50%))`
-
-  // border animation
-  heroBorder.animate([{ transform: "scaleY(0)" }, { transform: "scaleY(1)" }], {
-    duration: 400,
-    easing: "cubic-bezier(0.86, 0, 0.07, 1)",
-    fill: "forwards",
-    delay: 500,
-  })
-
-  // wait till text slides in
-  await waitForAnimation(
     heroText.animate(
       [
         {
@@ -202,38 +163,65 @@ async function heroTextAnimation() {
         },
       ],
       {
+        delay: -250,
         duration: 500,
         fill: "forwards",
       }
-    )
-  )
+    ),
+  ])
 
-  await delay(100)
-  // slide smaller text down, and title up
-  heroTitle.animate(
-    { transform: "translateY(0)" },
-    { duration: 300, easing: "ease", fill: "forwards" }
-  )
-  heroDesc.animate(
-    [
+  // step 4
+  await waitForAnimations([
+    heroBorder.animate(
+      [{ transform: "scaleY(0)" }, { transform: "scaleY(1)" }],
       {
-        offset: 0,
-        opacity: 0,
-        transform: "translateY(-25%)",
-        easing: "ease",
-      },
+        duration: 400,
+        easing: "cubic-bezier(0.86, 0, 0.07, 1)",
+        fill: "forwards",
+      }
+    ),
+    heroTitle.animate(
+      { transform: "translateY(0)" },
+      { duration: 300, easing: "ease", fill: "forwards", delay: 100 }
+    ),
+    heroDesc.animate(
+      [
+        {
+          offset: 0,
+          opacity: 0,
+          transform: "translateY(-50%)",
+          easing: "ease",
+        },
+        {
+          offset: 1,
+          opacity: 1,
+          transform: "translateY(0)",
+          easing: "ease",
+        },
+      ],
       {
-        offset: 1,
-        opacity: 1,
-        transform: "translateY(0)",
-        easing: "ease",
-      },
-    ],
-    {
-      duration: 300,
-      fill: "forwards",
-    }
-  )
+        duration: 300,
+        fill: "forwards",
+        delay: 100,
+      }
+    ),
+  ])
+
+  // using animation fill forward to retain new transforms and adding hover effects afterwards is kinda whack
+  // hacky fix: just replace the node
+  // 1) reset styling for elems that are about to be replaced
+  logoInit.style.left = "0%"
+  logoInit.style.translate = "0%"
+  logoMiddle.style.fillOpacity = 1
+  logoBottom.style.fillOpacity = 1
+  // 2) create node, and change id to use hover effects defined in css
+  const newNode = logoInit.cloneNode(true)
+  newNode.id = "logo"
+  // 3) remove old node and replace with new one
+  const parent = logoInit.parentNode
+  const sibling = logoInit.nextSibling
+  logoInit.remove()
+  parent.insertBefore(newNode, sibling)
 }
 
 // #region utility
