374c167c62b41a5b33ea1740cfc6f020f24d382d
vivCoding <vivCoding@gmail.com>
2024-01-11 22:21:18 -0500

    jazz

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,9 +2,13 @@ import { startTheme } from "./intro/bg"
 import { animateHero } from "./intro/hero"
 import { initSectionTransitions } from "./section-transitions"
 
+const reducedMotion = window.matchMedia(`(prefers-reduced-motion: safe)`).matches === true
+
 document.body.onload = () => {
   console.log("😎")
-  animateHero()
-  startTheme()
-  initSectionTransitions()
+  if (!reducedMotion) {
+    animateHero()
+    startTheme()
+    initSectionTransitions()
+  }
 }
