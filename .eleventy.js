bf50c60b1582a54f9924c7ea2268cd511af719d2
vivCoding <vivCoding@gmail.com>
2024-01-13 17:52:17 -0500

    jazz

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-// eleventy config
-module.exports = function (eleventyConfig) {
-  return {
-    dir: {
-      input: "pages",
-      output: "site",
-    },
-  }
-}
---
+++ eleventy.config.js
@@ -0,0 +1,30 @@
+// TODO consider using eleventy as the final builder/processor and using parcel only for js bundling
+// rather than having eleventy only for templates and parcel for bundling everything
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight)
+
+  // attempts to extracts a mini excerpt preview given a post
+  // uses the first paragraph as the excerpt's content
+  eleventyConfig.addFilter("excerpt", (postContent) => {
+    if (!postContent) return ""
+    const firstP = postContent.indexOf("<p>")
+    if (firstP >= 0) {
+      const endP = postContent.indexOf("</p>")
+      return postContent.substr(firstP, endP + 4)
+    }
+    return ""
+  })
+
+  return {
+    dir: {
+      input: "pages",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+    passthroughFileCopy: true,
+  }
+}
